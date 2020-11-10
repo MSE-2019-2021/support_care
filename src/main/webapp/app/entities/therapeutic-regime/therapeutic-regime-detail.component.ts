eccec235e49bcd0eabdab7c61d6e030b3d0cf2b9
Daniel Franco <dandrfranco@gmail.com>
2020-11-10 23:39:43 +0000

    Add delete button

--- src/main/webapp/app/entities/therapeutic-regime/therapeutic-regime-detail.component.ts
+++ src/main/webapp/app/entities/therapeutic-regime/therapeutic-regime-detail.component.ts
@@ -1,24 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { JhiEventManager } from 'ng-jhipster';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 import { ITherapeuticRegime } from 'app/shared/model/therapeutic-regime.model';
+import { TherapeuticRegimeService } from './therapeutic-regime.service';
+import { TherapeuticRegimeDeleteDialogComponent } from 'app/entities/therapeutic-regime/therapeutic-regime-delete-dialog.component';
 
 @Component({
   selector: 'custom-therapeutic-regime-detail',
   templateUrl: './therapeutic-regime-detail.component.html',
 })
-export class TherapeuticRegimeDetailComponent implements OnInit {
+export class TherapeuticRegimeDetailComponent implements OnInit, OnDestroy {
   therapeuticRegime: ITherapeuticRegime | null = null;
+  eventSubscriber?: Subscription;
 
-  constructor(protected activatedRoute: ActivatedRoute) {}
+  constructor(
+    protected activatedRoute: ActivatedRoute,
+    protected therapeuticRegimeService: TherapeuticRegimeService,
+    protected eventManager: JhiEventManager,
+    protected modalService: NgbModal
+  ) {}
 
   ngOnInit(): void {
     this.activatedRoute.data.subscribe(({ therapeuticRegime }) => {
       this.therapeuticRegime = therapeuticRegime;
     });
+    this.registerChangeInTherapeuticRegimes();
+  }
+
+  ngOnDestroy(): void {
+    if (this.eventSubscriber) {
+      this.eventManager.destroy(this.eventSubscriber);
+    }
   }
 
   previousState(): void {
     window.history.back();
+  }
+
+  registerChangeInTherapeuticRegimes(): void {
+    this.eventSubscriber = this.eventManager.subscribe('therapeuticRegimeViewModification', () => this.previousState());
+  }
+
+  delete(therapeuticRegime: ITherapeuticRegime): void {
+    const modalRef = this.modalService.open(TherapeuticRegimeDeleteDialogComponent, { size: 'lg', backdrop: 'static' });
+    modalRef.componentInstance.therapeuticRegime = therapeuticRegime;
+    modalRef.componentInstance.eventName = 'therapeuticRegimeViewModification';
   }
 }
