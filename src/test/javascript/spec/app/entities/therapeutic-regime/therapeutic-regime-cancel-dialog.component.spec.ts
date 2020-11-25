729242f420d1c62634c0ddfcfc454fda00f87c92
João <joao.m.letra@noesis.pt>
2020-11-25 20:04:55 +0000

    add test

---
+++ src/test/javascript/spec/app/entities/therapeutic-regime/therapeutic-regime-cancel-dialog.component.spec.ts
@@ -0,0 +1,68 @@
+jest.mock('ng-jhipster');
+jest.mock('@ng-bootstrap/ng-bootstrap');
+
+import { ComponentFixture, TestBed, inject, fakeAsync, tick } from '@angular/core/testing';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { of } from 'rxjs';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { JhiEventManager } from 'ng-jhipster';
+
+import { TherapeuticRegimeCancelDialogComponent } from 'app/entities/therapeutic-regime/therapeutic-regime-cancel-dialog.component';
+import { TherapeuticRegimeService } from 'app/entities/therapeutic-regime/therapeutic-regime.service';
+
+describe('Component Tests', () => {
+  describe('TherapeuticRegime Management cancel Component', () => {
+    let comp: TherapeuticRegimeCancelDialogComponent;
+    let fixture: ComponentFixture<TherapeuticRegimeCancelDialogComponent>;
+    let service: TherapeuticRegimeService;
+    let mockEventManager: JhiEventManager;
+    let mockActiveModal: NgbActiveModal;
+
+    beforeEach(() => {
+      TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
+        declarations: [TherapeuticRegimeCancelDialogComponent],
+        providers: [NgbActiveModal, JhiEventManager],
+      })
+        .overrideTemplate(TherapeuticRegimeCancelDialogComponent, '')
+        .compileComponents();
+      fixture = TestBed.createComponent(TherapeuticRegimeCancelDialogComponent);
+      comp = fixture.componentInstance;
+      comp.eventName = 'myEvent';
+      service = TestBed.inject(TherapeuticRegimeService);
+      mockEventManager = TestBed.inject(JhiEventManager);
+      mockActiveModal = TestBed.inject(NgbActiveModal);
+    });
+
+    describe('confirmCancel', () => {
+      it('Should call cancel service on confirmCancel', inject(
+        [],
+        fakeAsync(() => {
+          // GIVEN
+          spyOn(service, 'cancel').and.returnValue(of({}));
+
+          // WHEN
+          comp.confirmCancel(123);
+          tick();
+
+          // THEN
+          expect(service.cancel).toHaveBeenCalledWith(123);
+          expect(mockActiveModal.close).toHaveBeenCalled();
+          expect(mockEventManager.broadcast).toHaveBeenCalled();
+        })
+      ));
+
+      it('Should not call cancel service on clear', () => {
+        // GIVEN
+        spyOn(service, 'cancel');
+
+        // WHEN
+        comp.();
+
+        // THEN
+        expect(service.cancel).not.toHaveBeenCalled();
+        expect(mockActiveModal.dismiss).toHaveBeenCalled();
+      });
+    });
+  });
+});
