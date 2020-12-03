f512aeda41736e3ab122772675c75a0fb231032d
André <andrenunes.se@gmail.com>
2020-12-03 08:56:15 +0000

    MSEDO-191 - Remove unused import

--- src/main/webapp/app/app.module.ts
+++ src/main/webapp/app/app.module.ts
@@ -15,7 +15,6 @@ import { FooterComponent } from './layouts/footer/footer.component';
 import { PageRibbonComponent } from './layouts/profiles/page-ribbon.component';
 import { ActiveMenuDirective } from './layouts/navbar/active-menu.directive';
 import { ErrorComponent } from './layouts/error/error.component';
-import { NgMultiSelectDropDownModule } from 'ng-multiselect-dropdown';
 
 @NgModule({
   imports: [
