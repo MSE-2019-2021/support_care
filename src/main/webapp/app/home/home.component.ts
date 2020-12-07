3a71a1843d311dac32cf56608cc4b3feeb7948ea
Benjamim Ferreira <72458547+benjamim-ferreira@users.noreply.github.com>
2020-12-07 16:08:35 +0000

    creating routes for the homepage

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
 import { Subscription } from 'rxjs';
-
 import { AccountService } from 'app/core/auth/account.service';
 import { Account } from 'app/core/user/account.model';
 
