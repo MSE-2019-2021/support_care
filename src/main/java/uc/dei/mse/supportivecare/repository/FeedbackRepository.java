7611b81265ba1685c5bff85805c50c4d4db19449
Daniel Franco <dandrfranco@gmail.com>
2021-01-04 21:15:11 +0000

    Create Repository function to delete Feedback by Entity Name and Entity Id

--- src/main/java/uc/dei/mse/supportivecare/repository/FeedbackRepository.java
+++ src/main/java/uc/dei/mse/supportivecare/repository/FeedbackRepository.java
@@ -3,10 +3,13 @@ package uc.dei.mse.supportivecare.repository;
 import org.springframework.data.jpa.repository.*;
 import org.springframework.stereotype.Repository;
 import uc.dei.mse.supportivecare.domain.Feedback;
+import uc.dei.mse.supportivecare.domain.enumeration.EntityFeedback;
 
 /**
  * Spring Data SQL repository for the Feedback entity.
  */
 @SuppressWarnings("unused")
 @Repository
-public interface FeedbackRepository extends JpaRepository<Feedback, Long>, JpaSpecificationExecutor<Feedback> {}
+public interface FeedbackRepository extends JpaRepository<Feedback, Long>, JpaSpecificationExecutor<Feedback> {
+    void deleteByEntityNameAndEntityId(EntityFeedback entityFeedback, Long entityId);
+}
