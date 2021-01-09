00b04720a3b88b07e478ed5cbe3878e77e2ab99b
Benjamim Ferreira <72458547+benjamim-ferreira@users.noreply.github.com>
2021-01-09 02:52:27 +0000

    tests on the TRdetail component

--- src/main/webapp/app/entities/therapeutic-regime/detail/therapeutic-regime-detail.component.spec.ts
+++ src/main/webapp/app/entities/therapeutic-regime/detail/therapeutic-regime-detail.component.spec.ts
@@ -1,18 +1,26 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, inject, TestBed, tick } from '@angular/core/testing';
 import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
 import { TherapeuticRegime } from '../therapeutic-regime.model';
 
 import { TherapeuticRegimeDetailComponent } from './therapeutic-regime-detail.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { FeedbackService } from 'app/entities/feedback/service/feedback.service';
+import { Feedback } from 'app/entities/feedback/feedback.model';
+import { EntityFeedback } from 'app/entities/enumerations/entity-feedback.model';
 
 describe('Component Tests', () => {
   describe('TherapeuticRegime Management Detail Component', () => {
+    let mockActiveModal: NgbActiveModal;
     let comp: TherapeuticRegimeDetailComponent;
     let fixture: ComponentFixture<TherapeuticRegimeDetailComponent>;
+    let service: FeedbackService;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
+        imports: [HttpClientTestingModule],
         declarations: [TherapeuticRegimeDetailComponent],
         providers: [
           {
@@ -25,6 +33,7 @@ describe('Component Tests', () => {
         .compileComponents();
       fixture = TestBed.createComponent(TherapeuticRegimeDetailComponent);
       comp = fixture.componentInstance;
+      service = TestBed.inject(FeedbackService);
     });
 
     describe('OnInit', () => {
@@ -46,5 +55,28 @@ describe('Component Tests', () => {
         expect(comp.therapeuticRegime).toEqual(jasmine.objectContaining(null));
       });
     });
+    /*
+    describe('manageFeedback', () => {
+      it('Should enter in the managefeedback service', inject(
+        [],
+        fakeAsync(() => {
+          // GIVEN
+          const feedback = new Feedback();
+          feedback.entityName = EntityFeedback.THERAPEUTIC_REGIME;
+          feedback.entityId = comp.therapeuticRegime?.id;
+          feedback.thumb = true;
+
+          spyOn(service, 'manageFeedbackFromEntity').and.returnValue(of({}));
+
+          // WHEN
+          comp.manageFeedback(true);
+          tick();
+
+          // THEN
+          expect(service.manageFeedbackFromEntity(feedback)).toHaveBeenCalled();
+        })
+      ));
+    });
+*/
   });
 });
