69c757e9733add5a78331979a26acf16d263e8e3
Daniel Franco <dandrfranco@gmail.com>
2021-01-10 16:02:10 +0000

    Add Thumbs Up/Down to Symptoms

--- src/main/webapp/app/entities/symptom/detail/symptom-detail.component.spec.ts
+++ src/main/webapp/app/entities/symptom/detail/symptom-detail.component.spec.ts
@@ -4,13 +4,17 @@ import { ActivatedRoute } from '@angular/router';
 import { of } from 'rxjs';
 
 import { Symptom } from '../symptom.model';
-
 import { SymptomDetailComponent } from './symptom-detail.component';
+import { HttpHeaders, HttpResponse } from '@angular/common/http';
+import { FeedbackService } from 'app/entities/feedback/service/feedback.service';
+import { Feedback } from 'app/entities/feedback/feedback.model';
+import { EntityFeedback } from 'app/entities/enumerations/entity-feedback.model';
 
 describe('Component Tests', () => {
   describe('Symptom Management Detail Component', () => {
     let comp: SymptomDetailComponent;
     let fixture: ComponentFixture<SymptomDetailComponent>;
+    let service: FeedbackService;
 
     beforeEach(() => {
       TestBed.configureTestingModule({
@@ -27,6 +31,7 @@ describe('Component Tests', () => {
         .compileComponents();
       fixture = TestBed.createComponent(SymptomDetailComponent);
       comp = fixture.componentInstance;
+      service = TestBed.inject(FeedbackService);
     });
 
     describe('OnInit', () => {
@@ -46,6 +51,51 @@ describe('Component Tests', () => {
 
         // THEN
         expect(comp.symptom).toEqual(jasmine.objectContaining(null));
+      });
+    });
+
+    describe('load feedbacks', () => {
+      it('should load a page', () => {
+        // GIVEN
+        const headers = new HttpHeaders().append('link', 'link;link');
+        spyOn(service, 'query').and.returnValue(
+          of(
+            new HttpResponse({
+              body: [new Feedback(123)],
+              headers,
+            })
+          )
+        );
+
+        // WHEN
+        comp.loadPage(1);
+
+        // THEN
+        expect(service.query).toHaveBeenCalled();
+        expect(comp.feedbacks[0]).toEqual(jasmine.objectContaining({ id: 123 }));
+      });
+
+      it('should return id', () => {
+        // WHEN
+        const result = comp.trackId(1, new Feedback(123));
+
+        // THEN
+        expect(result).toEqual(123);
+      });
+    });
+
+    describe('manage feedback', () => {
+      it('should save thumb up', () => {
+        // GIVEN
+        const feedback = new Feedback();
+        feedback.entityName = EntityFeedback.SYMPTOM;
+        spyOn(service, 'manageFeedbackFromEntity').and.returnValue(of());
+
+        // WHEN
+        comp.manageFeedback(true);
+
+        // THEN
+        expect(service.manageFeedbackFromEntity).toHaveBeenCalled();
       });
     });
   });
