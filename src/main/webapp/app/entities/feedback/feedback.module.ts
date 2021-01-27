1e12e90ff15e613751f3302e35848069ef7f6c4c
DanielFran <dandrfranco@gmail.com>
2021-01-27 14:10:22 +0000

    Remove feedback options not used

--- src/main/webapp/app/entities/feedback/feedback.module.ts
+++ src/main/webapp/app/entities/feedback/feedback.module.ts
@@ -2,14 +2,12 @@ import { NgModule } from '@angular/core';
 
 import { SharedModule } from 'app/shared/shared.module';
 import { FeedbackComponent } from './list/feedback.component';
-import { FeedbackDetailComponent } from './detail/feedback-detail.component';
-import { FeedbackUpdateComponent } from './update/feedback-update.component';
 import { FeedbackDeleteDialogComponent } from './delete/feedback-delete-dialog.component';
 import { FeedbackRoutingModule } from './route/feedback-routing.module';
 
 @NgModule({
   imports: [SharedModule, FeedbackRoutingModule],
-  declarations: [FeedbackComponent, FeedbackDetailComponent, FeedbackUpdateComponent, FeedbackDeleteDialogComponent],
+  declarations: [FeedbackComponent, FeedbackDeleteDialogComponent],
   entryComponents: [FeedbackDeleteDialogComponent],
 })
 export class FeedbackModule {}
