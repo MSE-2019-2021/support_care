b11f2ba9e1fa67af1fb8c8386a9c7bc043235c07
André <andrenunes.se@gmail.com>
2021-02-03 20:21:28 +0000

    MSEDO-184 - Add methods to perform the marked as resolved action on feedbacks

--- src/main/webapp/app/entities/feedback/entity-list-card/feedback-entity-list-card.component.ts
+++ src/main/webapp/app/entities/feedback/entity-list-card/feedback-entity-list-card.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { IFeedback } from 'app/entities/feedback/feedback.model';
 import { DATE_TIME_FORMAT } from 'app/config/input.constants';
+import { FeedbackService } from 'app/entities/feedback/service/feedback.service';
 
 @Component({
   selector: 'custom-feedback-entity-list-card',
@@ -8,10 +9,21 @@ import { DATE_TIME_FORMAT } from 'app/config/input.constants';
 })
 export class FeedbackEntityListCardComponent {
   @Input() feedback: IFeedback;
+  @Input() feedbacks: IFeedback[];
+  @Input() i: number;
   dateFormat: string;
 
-  constructor() {
+  constructor(protected feedbackService: FeedbackService) {
+    this.i = 0;
+    this.feedbacks = [];
     this.feedback = {};
     this.dateFormat = DATE_TIME_FORMAT;
+  }
+
+  markFeedbackAsSolved(feedback: IFeedback, feedbacks: IFeedback[], i: number): void {
+    feedback.solved = true;
+    this.feedbackService.update(feedback).subscribe(() => {
+      this.feedbacks.splice(i, 1);
+    });
   }
 }
