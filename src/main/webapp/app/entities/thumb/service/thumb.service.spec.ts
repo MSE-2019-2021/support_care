eefc3bd698f2a02657f56c961e0ba1e3f5eb9964
DanielFran <dandrfranco@gmail.com>
2021-02-06 18:07:08 +0000

    Clean thumb service

--- src/main/webapp/app/entities/thumb/service/thumb.service.spec.ts
+++ src/main/webapp/app/entities/thumb/service/thumb.service.spec.ts
@@ -10,7 +10,6 @@ describe('Service Tests', () => {
   describe('Thumb Service', () => {
     let service: ThumbService;
     let httpMock: HttpTestingController;
-    let elemDefault: IThumb;
     let expectedResult: IThumb | IThumb[] | boolean | null;
 
     beforeEach(() => {
@@ -20,87 +19,9 @@ describe('Service Tests', () => {
       expectedResult = null;
       service = TestBed.inject(ThumbService);
       httpMock = TestBed.inject(HttpTestingController);
-
-      elemDefault = new Thumb(0, EntityFeedback.ACTIVE_SUBSTANCE, 0, false);
     });
 
     describe('Service methods', () => {
-      it('should find an element', () => {
-        const returnedFromService = Object.assign({}, elemDefault);
-
-        service.find(123).subscribe(resp => (expectedResult = resp.body));
-
-        const req = httpMock.expectOne({ method: 'GET' });
-        req.flush(returnedFromService);
-        expect(expectedResult).toMatchObject(elemDefault);
-      });
-
-      it('should create a Thumb', () => {
-        const returnedFromService = Object.assign(
-          {
-            id: 0,
-          },
-          elemDefault
-        );
-
-        const expected = Object.assign({}, returnedFromService);
-
-        service.create(new Thumb()).subscribe(resp => (expectedResult = resp.body));
-
-        const req = httpMock.expectOne({ method: 'POST' });
-        req.flush(returnedFromService);
-        expect(expectedResult).toMatchObject(expected);
-      });
-
-      it('should update a Thumb', () => {
-        const returnedFromService = Object.assign(
-          {
-            id: 1,
-            entityType: 'BBBBBB',
-            entityId: 1,
-            thumb: true,
-          },
-          elemDefault
-        );
-
-        const expected = Object.assign({}, returnedFromService);
-
-        service.update(expected).subscribe(resp => (expectedResult = resp.body));
-
-        const req = httpMock.expectOne({ method: 'PUT' });
-        req.flush(returnedFromService);
-        expect(expectedResult).toMatchObject(expected);
-      });
-
-      it('should return a list of Thumb', () => {
-        const returnedFromService = Object.assign(
-          {
-            id: 1,
-            entityType: 'BBBBBB',
-            entityId: 1,
-            thumb: true,
-          },
-          elemDefault
-        );
-
-        const expected = Object.assign({}, returnedFromService);
-
-        service.query().subscribe(resp => (expectedResult = resp.body));
-
-        const req = httpMock.expectOne({ method: 'GET' });
-        req.flush([returnedFromService]);
-        httpMock.verify();
-        expect(expectedResult).toContainEqual(expected);
-      });
-
-      it('should delete a Thumb', () => {
-        service.delete(123).subscribe(resp => (expectedResult = resp.ok));
-
-        const req = httpMock.expectOne({ method: 'DELETE' });
-        req.flush({ status: 200 });
-        expect(expectedResult);
-      });
-
       it('should update an entity Thumb', () => {
         const thumb = new Thumb();
         thumb.entityType = EntityFeedback.THERAPEUTIC_REGIME;
@@ -113,14 +34,11 @@ describe('Service Tests', () => {
       });
 
       it('should count thumbs for an entity Thumb', () => {
-        const returnedFromService = Object.assign(
-          {
-            countThumbUp: 1,
-            countThumbDown: 0,
-            thumb: true,
-          },
-          elemDefault
-        );
+        const returnedFromService = Object.assign({
+          countThumbUp: 1,
+          countThumbDown: 0,
+          thumb: true,
+        });
 
         const expected = Object.assign({}, returnedFromService);
 
--- src/main/webapp/app/entities/thumb/service/thumb.service.ts
+++ src/main/webapp/app/entities/thumb/service/thumb.service.ts
@@ -3,7 +3,6 @@ import { HttpClient, HttpResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { SERVER_API_URL } from 'app/app.constants';
-import { createRequestOption } from 'app/core/request/request-util';
 import { IThumb } from '../thumb.model';
 import { IThumbCount } from '../thumb-count.model';
 import { EntityFeedback } from 'app/entities/enumerations/entity-feedback.model';
@@ -16,27 +15,6 @@ export class ThumbService {
   public resourceUrl = SERVER_API_URL + 'api/thumbs';
 
   constructor(protected http: HttpClient) {}
-
-  create(thumb: IThumb): Observable<EntityResponseType> {
-    return this.http.post<IThumb>(this.resourceUrl, thumb, { observe: 'response' });
-  }
-
-  update(thumb: IThumb): Observable<EntityResponseType> {
-    return this.http.put<IThumb>(this.resourceUrl, thumb, { observe: 'response' });
-  }
-
-  find(id: number): Observable<EntityResponseType> {
-    return this.http.get<IThumb>(`${this.resourceUrl}/${id}`, { observe: 'response' });
-  }
-
-  query(req?: any): Observable<EntityArrayResponseType> {
-    const options = createRequestOption(req);
-    return this.http.get<IThumb[]>(this.resourceUrl, { params: options, observe: 'response' });
-  }
-
-  delete(id: number): Observable<HttpResponse<{}>> {
-    return this.http.delete(`${this.resourceUrl}/${id}`, { observe: 'response' });
-  }
 
   countThumbsFromEntity(entityType: EntityFeedback, entityId: number): Observable<EntityResponseType> {
     return this.http.get<IThumbCount>(`${this.resourceUrl}/${entityType.valueOf()}/${entityId}/count`, { observe: 'response' });
